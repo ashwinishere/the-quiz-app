3bdc2c2cbd56633efb0902e9b946f3f174926df4
Aswin <aswin.harikumar@yahoo.com>
2020-07-07 23:36:12 +0530

    complete game

--- src/index.js
+++ src/index.js
@@ -2,35 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-
-  render() {
+const Square = (props) =>  {
     return (
-      <button onClick={() => this.props.onClick()} className="square">
-        {this.props.value}
+      <button onClick={() => props.onClick()} className="square">
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: new Array(9).fill(null)
-    }
-  }
+
   handleClick(i) {
 const squares = this.state.squares.slice();
-squares[i] = 'X';
-this.setState({squares});
+if (calculateWinner(squares) || squares[i]) {
+  return;
+}
+squares[i] = this.state.isXnext ? 'X' : 'O';
+this.setState({squares,isXnext: !this.state.isXnext});
   }
   renderSquare(i) {
     return <Square onClick={() => this.handleClick(i)} value={this.state.squares[i]} />;
   }
 
   render() {
-    const status = "Next player: X";
+    const isWinner = calculateWinner(this.state.squares);
+    let status;
+    if (isWinner) {
+     status =  `Winner is ${isWinner}`; 
+    } else {
+      
+    status = `Next player: ${this.state.isXnext ? 'X': 'O'}`;
+    }
 
     return (
       <div>
@@ -56,6 +58,13 @@ this.setState({squares});
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: new Array(9).fill(null),
+      isXnext: true
+    }
+  }
   render() {
     return (
       <div className="game">
@@ -70,7 +79,25 @@ class Game extends React.Component {
     );
   }
 }
-
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
